cd2b02f6b60e054380a3efb75e64fbedfccc9e25
Brian Peck <bpeck@iastate.edu>
2014-03-24 15:03:20 -0500

    Add Lab 5

---
+++ lab5/code_5a/scheduling.c
@@ -0,0 +1,97 @@
+/*******************************************************************************
+*
+* CprE 308 Scheduling Lab
+*
+* scheduling.c
+*******************************************************************************/
+
+#define NUM_PROCESSES 20
+
+struct process
+{
+  /* Values initialized for each process */
+  int arrivaltime;  /* Time process arrives and wishes to start */
+  int runtime;      /* Time process requires to complete job */
+  int priority;     /* Priority of the process */
+
+  /* Values algorithm may use to track processes */
+  int starttime;
+  int endtime;
+  int flag;
+  int remainingtime;
+};
+
+/* Forward declarations of Scheduling algorithms */
+void first_come_first_served(struct process *proc);
+void shortest_remaining_time(struct process *proc);
+void round_robin(struct process *proc);
+void round_robin_priority(struct process *proc);
+
+int main()
+{
+  int i;
+  struct process proc[NUM_PROCESSES],      /* List of processes */
+                 proc_copy[NUM_PROCESSES]; /* Backup copy of processes */
+
+  /* Seed random number generator */
+  /*srand(time(0));*/  /* Use this seed to test different scenarios */
+  srand(0xC0FFEE);     /* Used for test to be printed out */
+
+  /* Initialize process structures */
+  for(i=0; i<NUM_PROCESSES; i++)
+  {
+    proc[i].arrivaltime = rand()%100;
+    proc[i].runtime = (rand()%30)+10;
+    proc[i].priority = rand()%3;
+    proc[i].starttime = 0;
+    proc[i].endtime = 0;
+    proc[i].flag = 0;
+    proc[i].remainingtime = 0;
+  }
+
+  /* Show process values */
+  printf("Process\tarrival\truntime\tpriority\n");
+  for(i=0; i<NUM_PROCESSES; i++)
+    printf("%d\t%d\t%d\t%d\n", i, proc[i].arrivaltime, proc[i].runtime,
+           proc[i].priority);
+
+  /* Run scheduling algorithms */
+  printf("\n\nFirst come first served\n");
+  memcpy(proc_copy, proc, NUM_PROCESSES * sizeof(struct process));
+  first_come_first_served(proc_copy);
+
+  printf("\n\nShortest remaining time\n");
+  memcpy(proc_copy, proc, NUM_PROCESSES * sizeof(struct process));
+  shortest_remaining_time(proc_copy);
+
+  printf("\n\nRound Robin\n");
+  memcpy(proc_copy, proc, NUM_PROCESSES * sizeof(struct process));
+  round_robin(proc_copy);
+
+  printf("\n\nRound Robin with priority\n");
+  memcpy(proc_copy, proc, NUM_PROCESSES * sizeof(struct process));
+  round_robin_priority(proc_copy);
+
+  return 0;
+}
+
+void first_come_first_served(struct process *proc)
+{
+  /* Implement scheduling algorithm here */
+}
+
+void shortest_remaining_time(struct process *proc)
+{
+  /* Implement scheduling algorithm here */
+}
+
+void round_robin(struct process *proc)
+{
+  /* Implement scheduling algorithm here */
+}
+
+void round_robin_priority(struct process *proc)
+{
+  /* Implement scheduling algorithm here */
+}
+
